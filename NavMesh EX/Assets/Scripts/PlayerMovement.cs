9b47eaaa3d692a8f920cf6606c60ea26cf9a5d6e
CorpseParty911 <49690591+CorpseParty911@users.noreply.github.com>
2021-03-14 21:10:44 -0700

    Add Unity Files

---
+++ NavMesh EX/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject target;
+    public bool pursue;
+
+    private Camera cam;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pursue)
+        {
+            agent.SetDestination(target.transform.position);
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
